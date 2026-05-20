a621334637ea217654b877263260dd802ceaa1bc
nandaabiii <160771519+nandaabiii@users.noreply.github.com>
2026-05-20 21:04:43 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# ============================================================
+# main.py  —  Entry Point
+# Sistem Informasi Akademik Mahasiswa
+# ============================================================
+#
+# Cara jalankan:
+#   pip install customtkinter
+#   python main.py
+#
+# Login default:
+#   Username : admin
+#   Password : 123
+# ============================================================
+
+from ui import App
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
